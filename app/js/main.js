5e4c50ddca873040a50966cbb28b394ba18686de
Dobrodei <Dobrodei.98@gmail.com>
2021-10-07 00:53:53 +0300

    add slider and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
 $(function () {
   $('.header__search-catalog').on('click', function (event) {
     event.preventDefault();
-    $('.header__search-menu').toggleClass('head')
+    $('.header__search-menu').toggleClass('header__search-menu--active')
+    $('.header__search-catalog').toggleClass('header__search-catalog--active')
+  })
+
+  $('.slider').slick({
+    adaptiveHeight: true,
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt=""></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt=""></button>',
   })
 })
