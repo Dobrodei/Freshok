5bee3c056d70b891ce4db08f80a04be58207be67
Dobrodei <Dobrodei.98@gmail.com>
2021-10-09 20:06:10 +0300

    add filter

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,26 @@
 $(function () {
-  $('.header__search-catalog').on('click', function (event) {
+  $('.header__search-catalog ').on('click', function (event) {
     event.preventDefault();
     $('.header__search-menu').toggleClass('header__search-menu--active')
     $('.header__search-catalog').toggleClass('header__search-catalog--active')
   })
+
+  $('.cart__menu-close').on('click', function () {
+    $('.cart__menu').toggleClass('cart__menu--active')
+    $('body').removeClass('body--lock')
+  })
+  
+  $('.header__user-cart').on('click', function () {
+    $('.cart__menu').toggleClass('cart__menu--active');
+    $('body').toggleClass('body--lock')
+  })
+
 
   $('.slider').slick({
     adaptiveHeight: true,
     nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt=""></button>',
     prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt=""></button>',
   })
+
+  var mixer = mixitup('.filter__product');
 })
