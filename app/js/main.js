dd250b341ce02c762cff9b82aa5e09edf0d5d28c
Dobrodei <Dobrodei.98@gmail.com>
2021-10-11 20:33:02 +0300

    fix bug

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,25 @@
 $(function () {
+
   $('.header__search-catalog ').on('click', function (event) {
     event.preventDefault();
     $('.header__search-menu').toggleClass('header__search-menu--active')
     $('.header__search-catalog').toggleClass('header__search-catalog--active')
   })
 
+  $(document).on('click', function (e) { // событие клика по веб-документу
+    var div = $(".header__search-menu--active"); // тут указываем ID элемента
+    if (!div.is(e.target) // если клик был не по нашему блоку
+      &&
+      div.has(e.target).length === 0) { // и не по его дочерним элементам
+      div.css('display: none'); // скрываем его
+    }
+  });
+
   $('.cart__menu-close').on('click', function () {
     $('.cart__menu').toggleClass('cart__menu--active')
     $('body').removeClass('body--lock')
   })
-  
+
   $('.header__user-cart').on('click', function () {
     $('.cart__menu').toggleClass('cart__menu--active');
     $('body').toggleClass('body--lock')
@@ -18,9 +28,10 @@ $(function () {
 
   $('.slider').slick({
     adaptiveHeight: true,
-    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt=""></button>',
-    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img class="slider-arrow" src="images/icons/arrow-right.svg" alt=""></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img class="slider-arrow" src="images/icons/arrow-left.svg" alt=""></button>',
   })
 
   var mixer = mixitup('.filter__product');
+  var sale = mixitup('.sale__product')
 })
