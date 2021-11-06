6ec383d97486fa44cb673f8c48763536cdb0fc76
Dobrodei <Dobrodei.98@gmail.com>
2021-11-07 00:06:18 +0300

    full catalog page

--- gulpfile.js
+++ gulpfile.js
@@ -57,6 +57,7 @@ function scripts() {
       'node_modules/mixitup/dist/mixitup.js',
       'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
       'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+      'node_modules/simplebar/dist/simplebar.js',
       'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
